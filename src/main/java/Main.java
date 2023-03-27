37953d72fd4e23d09627a3dba05d5599574ebd13
Viktar Kudasau <viktar.kudasau@gmail.com>
2023-03-27 17:13:59 +0300

    JUnit5

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String result = MainClass.justExample("1256");
+        System.out.println("String after replace " + result);
+    }
+}
---
+++ src/main/java/MainClass.java
@@ -0,0 +1,13 @@
+public class MainClass {
+    public static String justExample(String string){
+        System.out.println(string);
+        if (string.contains("3")){
+            throw new NumberFormatException();
+        }
+        if (string.contains("4")){
+            return string.replace("1", "3");
+        }
+        String result = string.replace("1", "2");
+        return result;
+    }
+}
